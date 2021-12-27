71863441ba9887d1ddd99596574dc263d58c365b
Khlomov Anton <antonhlomov@yandex.ru>
2021-12-27 22:35:13 +0100

    Добавлены кнопки, карта, поля ввода текста

---
+++ Barcelona guide/Utils.swift
@@ -0,0 +1,184 @@
+//
+//  Utils.swift
+//  Barcelona guide
+//
+//  Created by Anton Khlomov on 27/12/2021.
+//
+
+import UIKit
+
+class CustomTextField: UITextField{
+    let padding: CGFloat
+    init(padding:CGFloat) {
+        self.padding = padding
+        super.init(frame: .zero)
+        
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: padding, dy: 0)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: padding, dy: 0)
+    }
+    override var intrinsicContentSize: CGSize{
+        return .init(width: 0, height: 50)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+// функция для лейбл
+
+extension UILabel{
+    class func setupLabel(title: String) -> UILabel {
+        
+        let text = UILabel()
+        text.text = title
+        text.font = UIFont.systemFont(ofSize: 16)
+        
+        return text
+    }
+}
+
+// функция для поля текст филд
+extension UITextField{
+    class func setupTextField(title: String, hideText: Bool) -> UITextField {
+        
+        let tf = CustomTextField(padding: 16)
+        tf.placeholder = title
+        tf.backgroundColor = UIColor(white: 1, alpha: 0.7)
+        tf.layer.cornerRadius = 5
+        tf.font = UIFont .systemFont(ofSize: 16)
+        tf.textColor = .darkText
+        tf.isSecureTextEntry = hideText         // скрытие пороля
+        return tf
+    }
+}
+
+// функция для кнопки
+
+extension UIButton{
+    class func setupButton(title: String, color: UIColor, activation: Bool, invisibility: Bool ) -> UIButton {
+        let button = UIButton(type: .system)
+        button.setTitle(title, for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = color
+        button.layer.cornerRadius = 30/2  // скругляем кнопку
+        button.isEnabled = activation   //диактивация кнопки изначально кнопка не активна (активна после заполнения всех полей)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        button.isHidden = invisibility
+        
+//
+//        button.layer.borderColor = color.cgColor
+//        button.backgroundColor = .white
+//        button.layer.borderWidth = 3
+        
+        
+        return button
+    }
+}
+
+
+
+
+// функция для цветов в приложении
+extension UIColor{
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+    
+}
+
+
+
+//  структура констрейнов отступов и зависимостей(верх, лево, низ, право, ширина, высота)
+struct AnchoredConstraints {
+    var top, leading, bottom, trailing, width, height: NSLayoutConstraint?
+}
+
+extension UIView{
+    
+    @discardableResult
+    func anchor(top: NSLayoutYAxisAnchor?, leading: NSLayoutXAxisAnchor?, bottom: NSLayoutYAxisAnchor?, trailing: NSLayoutXAxisAnchor?,pading: UIEdgeInsets = .zero, size: CGSize = .zero) -> AnchoredConstraints {
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        var anchoredConstraints = AnchoredConstraints()
+        
+        if let top = top {
+                   anchoredConstraints.top = topAnchor.constraint(equalTo: top, constant: pading.top)
+               }
+        
+        if let leading = leading {
+            anchoredConstraints.leading = leadingAnchor.constraint(equalTo: leading, constant: pading.left)
+        }
+        
+        if let bottom = bottom {
+                   anchoredConstraints.bottom = bottomAnchor.constraint(equalTo: bottom, constant: -pading.bottom)
+               }
+        if let trailing = trailing{
+                   anchoredConstraints.trailing = trailingAnchor.constraint(equalTo: trailing, constant: -pading.right)
+               }
+        if size.width != 0 {
+            anchoredConstraints.width = widthAnchor.constraint(equalToConstant: size.width)
+            
+        }
+        
+        if size.height != 0 {
+            anchoredConstraints.height = heightAnchor.constraint(equalToConstant: size.height)
+        }
+        
+        [anchoredConstraints.top, anchoredConstraints.leading, anchoredConstraints.bottom, anchoredConstraints.trailing, anchoredConstraints.width, anchoredConstraints.height].forEach{
+            $0?.isActive = true}
+        
+        return anchoredConstraints
+        
+        }
+        
+    
+
+
+    func fillSuperview(pading: UIEdgeInsets = .zero)  {
+    translatesAutoresizingMaskIntoConstraints = false
+        if let superviewTopAnchor = superview?.topAnchor{
+            topAnchor.constraint(equalTo: superviewTopAnchor, constant: pading.top).isActive = true
+        }
+        
+        if let superviewBottomAnchor = superview?.bottomAnchor{
+            topAnchor.constraint(equalTo: superviewBottomAnchor, constant: pading.bottom).isActive = true
+        }
+        
+        if let superviewLeadingAnchor = superview?.leadingAnchor{
+            leadingAnchor.constraint(equalTo: superviewLeadingAnchor, constant: pading.left ).isActive = true
+        }
+        
+        if let superviewTrailingAnchor = superview?.trailingAnchor{
+            trailingAnchor.constraint(equalTo: superviewTrailingAnchor, constant: pading.right).isActive = true
+        }
+    }
+
+    func centerInSuperview(size: CGSize = .zero)  {
+    translatesAutoresizingMaskIntoConstraints = false
+        if let superviwCenterXAnchor = superview? .centerXAnchor{
+            centerXAnchor.constraint(equalTo: superviwCenterXAnchor).isActive = true
+        }
+        
+        if let superviwCenterYAnchor = superview? .centerYAnchor{
+            centerYAnchor.constraint(equalTo: superviwCenterYAnchor).isActive = true
+        }
+        
+        if size.width != 0 {
+            widthAnchor.constraint(equalToConstant: size.width).isActive = true
+        }
+        
+        if size.height != 0 {
+            heightAnchor.constraint(equalToConstant: size.width).isActive = true
+        }
+        
+}
+    
+    
+}
+
--- Barcelona guide/ViewController.swift
+++ Barcelona guide/ViewController.swift
@@ -6,13 +6,105 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
-
+    
+    fileprivate let mapView: MKMapView = {
+        let mapView = MKMapView()
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        return mapView
+    }()
+    
+  
+    
+    fileprivate let firstPlaceTextfield = UITextField.setupTextField(title: "First place..", hideText: false)
+    
+    fileprivate let secondPlaceTextField = UITextField.setupTextField(title: "Second place..", hideText: false)
+    
+    lazy var stackTextFieldView = UIStackView(arrangedSubviews: [firstPlaceTextfield,secondPlaceTextField])
+    // создание кнопку "Регистрация.."
+    fileprivate let addAdressButton = UIButton.setupButton(title: "Add", color: UIColor.rgb(red: 190, green: 140, blue: 196),activation: true,invisibility: false)
+    // создание кнопку "Регистрация.."
+    fileprivate let routeButton = UIButton.setupButton(title: "Route", color: UIColor.rgb(red: 190, green: 140, blue: 196),activation: true,invisibility: false)
+    // создание кнопку "Регистрация.."
+    fileprivate let resetButton = UIButton.setupButton(title: "Reset", color: UIColor.rgb(red: 190, green: 140, blue: 196),activation: true,invisibility: false)
+    
+    lazy var stackButtonView = UIStackView(arrangedSubviews: [addAdressButton,routeButton,resetButton])
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
-        // Do any additional setup after loading the view.
+        
+        hadleres()
+        configureViewComponents()
+        setupTapGesture()
+     
+    }
+    
+    override func viewDidLayoutSubviews() {
+      super.viewDidLayoutSubviews()
+        configureViewComponents()
+    }
+    
+    
+    
+    func hadleres(){
+    
+    addAdressButton.addTarget(self, action: #selector(touchAddAdress), for: .touchUpInside)
+    routeButton.addTarget(self, action: #selector(touchRouteButton), for: .touchUpInside)
+    resetButton.addTarget(self, action: #selector(touchResetButton), for: .touchUpInside)
+ 
+    }
+    
+    
+    
+    fileprivate func configureViewComponents(){
+       
+        view.addSubview(mapView)
+        mapView.anchor(top: view.topAnchor, leading: view.leadingAnchor, bottom: view.bottomAnchor, trailing: view.trailingAnchor, pading: .init(top: 0, left: 0, bottom: 0, right: 0), size: .init(width: 0, height: 0))
+        
+    
+        
+        stackTextFieldView.axis = .vertical
+        stackTextFieldView.spacing = 20
+        stackTextFieldView.distribution = .fillEqually  // для корректного отображения
+        
+        view.addSubview(stackTextFieldView)
+        stackTextFieldView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.safeAreaLayoutGuide.leadingAnchor, bottom: nil, trailing: view.safeAreaLayoutGuide.trailingAnchor, pading: .init(top: 10, left: 10, bottom: 0, right: 10), size: .init(width: 0, height: 100))
+       // stackTextFieldView.centerXAnchor.constraint(equalTo: addresses.centerXAnchor).isActive = true //выстовляет по середине экрана
+      //  stackTextFieldView.centerYAnchor.constraint(equalTo: addresses.centerYAnchor).isActive = true
+        
+        
+
+        stackButtonView.axis = .horizontal
+        stackButtonView.spacing = 30
+        stackButtonView.distribution = .fillEqually  // для корректного отображения
+        
+        view.addSubview(stackButtonView)
+        stackButtonView.anchor(top: nil, leading: view.safeAreaLayoutGuide.leadingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: view.safeAreaLayoutGuide.trailingAnchor, pading: .init(top: 0, left: 10 , bottom: 0, right: 10), size: .init(width: 0, height: 0))
+        
+    }
+    
+    fileprivate func setupTapGesture(){
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTapDismiss)))
+      
+    }
+    @objc fileprivate func handleTapDismiss(){
+        view.endEditing(true)
+    }
+    
+    
+    
+    @objc fileprivate func touchAddAdress(){
+        print("touchAddAdress")
+    }
+    @objc fileprivate func touchRouteButton(){
+        print("touchRouteButton")
+    }
+    @objc fileprivate func touchResetButton(){
+        print("touchResetButton")
     }
 
 
