4aab1ffdb9735e5e187da8d8d48b7e2b879ab764
Khlomov Anton <antonhlomov@yandex.ru>
2021-12-28 13:57:54 +0100

    исправил ошибку при изменение значений в текстовых полях

--- Barcelona guide/ViewController.swift
+++ Barcelona guide/ViewController.swift
@@ -25,13 +25,15 @@ class ViewController: UIViewController {
     
     lazy var stackTextFieldView = UIStackView(arrangedSubviews: [firstPlaceTextfield,secondPlaceTextField])
     
+    fileprivate let addAdressButton = UIButton.setupButton(title: "+", color: UIColor.rgb(red: 255, green: 255, blue: 255),activation: true,invisibility: false, laeyerRadius: 30/2, alpha: 0.5)
+    
     fileprivate let styleMap = UIButton.setupButton(title: "[]", color: UIColor.rgb(red: 190, green: 140, blue: 196),activation: true,invisibility: false, laeyerRadius: 40/2, alpha: 0.7)
     
-    fileprivate let addAdressButton = UIButton.setupButton(title: "+", color: UIColor.rgb(red: 190, green: 140, blue: 196),activation: true,invisibility: false, laeyerRadius: 50/2, alpha: 0.8)
+    fileprivate let addAdressButton1 = UIButton.setupButton(title: "+", color: UIColor.rgb(red: 190, green: 140, blue: 196),activation: true,invisibility: false, laeyerRadius: 50/2, alpha: 0.8)
     
-    fileprivate let userLocationButtonCircle = UIButton.setupButton(title: "*", color: UIColor.rgb(red: 190, green: 140, blue: 196),activation: true,invisibility: false, laeyerRadius: 50/2, alpha: 0.8)
+    fileprivate let userLocationButtonCircle = UIButton.setupButton(title: "*", color: UIColor.rgb(red: 31, green: 152, blue: 233),activation: true,invisibility: false, laeyerRadius: 50/2, alpha: 0.6)
     
-    lazy var circleButtonView = UIStackView(arrangedSubviews: [userLocationButtonCircle,addAdressButton])
+    lazy var circleButtonView = UIStackView(arrangedSubviews: [userLocationButtonCircle,addAdressButton1])
   
     fileprivate let routeButton = UIButton.setupButton(title: "Route", color: UIColor.rgb(red: 190, green: 140, blue: 196),activation: true,invisibility: false, laeyerRadius: 30/2, alpha: 1)
    
@@ -72,7 +74,11 @@ class ViewController: UIViewController {
         
         view.addSubview(stackTextFieldView)
         stackTextFieldView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.safeAreaLayoutGuide.leadingAnchor, bottom: nil, trailing: view.safeAreaLayoutGuide.trailingAnchor, pading: .init(top: 10, left: 10, bottom: 0, right: 10), size: .init(width: 0, height: 100))
-
+        
+        
+        view.addSubview(addAdressButton)
+        addAdressButton.anchor(top: stackTextFieldView.bottomAnchor, leading: nil, bottom: nil, trailing: nil, pading: .init(top: 22, left: 0, bottom: 0, right: 0), size: .init(width: 30, height: 30))
+        addAdressButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true //выстовляет по середине экрана
         
         view.addSubview(styleMap)
         styleMap.anchor(top: stackTextFieldView.bottomAnchor, leading: nil, bottom: nil, trailing: view.safeAreaLayoutGuide.trailingAnchor, pading: .init(top: 20, left: 0, bottom: 0, right: 10), size: .init(width: 40, height: 40))
@@ -98,8 +104,8 @@ class ViewController: UIViewController {
     // проверяем поля на заполненность
     func hadleres(){
     
-        firstPlaceTextfield.addTarget(self, action: #selector(formValidation), for: .editingDidEnd )
-        secondPlaceTextField.addTarget(self, action: #selector(formValidation), for: .editingDidEnd)
+        firstPlaceTextfield.addTarget(self, action: #selector(formValidationFirst), for: .editingDidEnd )
+        secondPlaceTextField.addTarget(self, action: #selector(formValidationSecond), for: .editingDidEnd)
     
         addAdressButton.addTarget(self, action: #selector(touchAddAdress), for: .touchUpInside)
         routeButton.addTarget(self, action: #selector(touchRouteButton), for: .touchUpInside)
@@ -111,24 +117,28 @@ class ViewController: UIViewController {
     
     
     // проверяем поля на заполненность
-    @objc fileprivate func formValidation() {
+    @objc fileprivate func formValidationFirst() {
         guard
-            firstPlaceTextfield.hasText,
-            secondPlaceTextField.hasText
-              
+            firstPlaceTextfield.hasText
         else {
-           // self.loginButton.isEnabled = false
-           // self.loginButton.backgroundColor = UIColor.rgb(red: 190, green: 140, blue: 196)
             return
         }
-        
         guard let first = firstPlaceTextfield.text else {return}
-        guard let second = secondPlaceTextField.text else {return}
         // получаем текст из алерта  отпраляем его в функцию и получаем анатацию и точки на карте
-        setupPlacemark(adressPlace: first)
-        setupPlacemark(adressPlace: second)
-      // loginButton.isEnabled = true
-      // loginButton.backgroundColor = UIColor.rgb(red: 170, green: 92, blue: 178)
+        setupPlacemark(adressPlace: first, mark: "ferstText")
+    
+    }
+    
+    // проверяем поля на заполненность
+    @objc fileprivate func formValidationSecond() {
+        guard
+            secondPlaceTextField.hasText
+        else {
+            return
+        }
+        guard let second = secondPlaceTextField.text else {return}
+        setupPlacemark(adressPlace: second, mark: "secondText")
+      
     }
     
    
@@ -138,7 +148,7 @@ class ViewController: UIViewController {
     @objc fileprivate func touchAddAdress(){
         alertAddAdress(title: "Add one more place", placeholder: "Add adres") {  [self] (text) in
         // получаем текст из алерта  отпраляем его в функцию и получаем анатацию и точки на карте
-        setupPlacemark(adressPlace: text)
+            setupPlacemark(adressPlace: text, mark: "default")
         }
         
     
@@ -188,7 +198,7 @@ class ViewController: UIViewController {
     }
     
     //MARK: - put point for map.
-    private func  setupPlacemark(adressPlace: String){
+    private func  setupPlacemark(adressPlace: String, mark: String){
         let geocoder = CLGeocoder()
         geocoder.geocodeAddressString(adressPlace) { [self] (placemarks, error) in
             if let error = error{
@@ -206,11 +216,26 @@ class ViewController: UIViewController {
             guard let placemarkLocation = placemark?.location else {return}
             anotation.coordinate = placemarkLocation.coordinate
             
-            annotationsArray.append(anotation)
-            
-         //  if annotationsArray.count > 2 {
-         //      // появиться кнопки если в массиве точек 3 точки адреса
-         //  }
+            switch mark{
+            case "ferstText" :
+                if annotationsArray.indices.contains(0) {
+                   mapView.removeAnnotation(annotationsArray[0])
+                   annotationsArray[0] = anotation
+                } else {
+                    annotationsArray.append(anotation)
+                }
+                
+            case "secondText":
+                if annotationsArray.indices.contains(1) {
+                   mapView.removeAnnotation(annotationsArray[1])
+                   annotationsArray[1] = anotation
+                } else {
+                   annotationsArray.append(anotation)
+                }
+            default:
+                annotationsArray.append(anotation)
+            }
+         
             mapView.showAnnotations(annotationsArray, animated: true)
            
         }
