e350fa0dfcd433da1c8fd82aa2aa54506d339cba
Khlomov Anton <antonhlomov@yandex.ru>
2021-12-28 11:16:03 +0100

    добавил swtсh для переключения стилей карты

--- Barcelona guide/ViewController.swift
+++ Barcelona guide/ViewController.swift
@@ -24,11 +24,13 @@ class ViewController: UIViewController {
     fileprivate let secondPlaceTextField = UITextField.setupTextField(title: "Second place..", hideText: false)
     
     lazy var stackTextFieldView = UIStackView(arrangedSubviews: [firstPlaceTextfield,secondPlaceTextField])
-    // создание кнопку "Регистрация.."
+    
+    fileprivate let styleMap = UIButton.setupButton(title: "[]", color: UIColor.rgb(red: 190, green: 140, blue: 196),activation: true,invisibility: false)
+  
     fileprivate let addAdressButton = UIButton.setupButton(title: "Add", color: UIColor.rgb(red: 190, green: 140, blue: 196),activation: true,invisibility: false)
-    // создание кнопку "Регистрация.."
+  
     fileprivate let routeButton = UIButton.setupButton(title: "Route", color: UIColor.rgb(red: 190, green: 140, blue: 196),activation: true,invisibility: false)
-    // создание кнопку "Регистрация.."
+   
     fileprivate let resetButton = UIButton.setupButton(title: "Reset", color: UIColor.rgb(red: 190, green: 140, blue: 196),activation: true,invisibility: false)
     
     lazy var stackButtonView = UIStackView(arrangedSubviews: [addAdressButton,routeButton,resetButton])
@@ -58,7 +60,7 @@ class ViewController: UIViewController {
         view.addSubview(mapView)
         mapView.anchor(top: view.topAnchor, leading: view.leadingAnchor, bottom: view.bottomAnchor, trailing: view.trailingAnchor, pading: .init(top: 0, left: 0, bottom: 0, right: 0), size: .init(width: 0, height: 0))
         
-    
+       
         
         stackTextFieldView.axis = .vertical
         stackTextFieldView.spacing = 20
@@ -69,6 +71,8 @@ class ViewController: UIViewController {
        // stackTextFieldView.centerXAnchor.constraint(equalTo: addresses.centerXAnchor).isActive = true //выстовляет по середине экрана
       //  stackTextFieldView.centerYAnchor.constraint(equalTo: addresses.centerYAnchor).isActive = true
         
+        view.addSubview(styleMap)
+        styleMap.anchor(top: stackTextFieldView.bottomAnchor, leading: nil, bottom: nil, trailing: view.safeAreaLayoutGuide.trailingAnchor, pading: .init(top: 20, left: 0, bottom: 0, right: 10), size: .init(width: 30, height: 30))
         
 
         stackButtonView.axis = .horizontal
@@ -89,6 +93,7 @@ class ViewController: UIViewController {
         addAdressButton.addTarget(self, action: #selector(touchAddAdress), for: .touchUpInside)
         routeButton.addTarget(self, action: #selector(touchRouteButton), for: .touchUpInside)
         resetButton.addTarget(self, action: #selector(touchResetButton), for: .touchUpInside)
+        styleMap.addTarget(self, action: #selector(changeStyleMap), for: .touchUpInside)
     
     }
     
@@ -140,6 +145,8 @@ class ViewController: UIViewController {
         
     }
     @objc fileprivate func touchResetButton(){
+        self.mapView.mapType = .satellite
+        
         firstPlaceTextfield.text = ""
         secondPlaceTextField.text = ""
         mapView.removeOverlays(mapView.overlays)
@@ -148,8 +155,27 @@ class ViewController: UIViewController {
         
         // здесь можно убрать активность кнопки
      
+    }
+    
+    @objc fileprivate func changeStyleMap(){
         
- 
+        switch mapView.mapType {
+        case .standard:
+            mapView.mapType = .satellite
+        case .satellite:
+            mapView.mapType = .hybrid
+        case .hybrid:
+            mapView.mapType = .satelliteFlyover
+        case .satelliteFlyover:
+            mapView.mapType = .hybridFlyover
+        case .hybridFlyover:
+            mapView.mapType = .mutedStandard
+        case .mutedStandard:
+            mapView.mapType = .standard
+        @unknown default:
+            mapView.mapType = .standard
+        }
+
     }
     
     //MARK: - put point for map.
