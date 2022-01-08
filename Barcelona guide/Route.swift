b7d840bb4b3a8a959790cdc8b9b7456b894497b1
Khlomov Anton <antonhlomov@yandex.ru>
2022-01-08 12:44:19 +0100

    Создал модель class Route

--- Barcelona guide/Route.swift
+++ Barcelona guide/Route.swift
@@ -11,33 +11,22 @@ import MapKit
 
 class Route {
     
-    var idUser: String!
-    var imageUser: String!
-    var nameUser: String!
-    var shurnameUser: String!
+    var idGuide: String!
+    var imageGuide: String!
+    var nameGuide: String!
+    var shurnameGuide: String!
+    var numberPhoneGuide: String!
     
     var idRoute: String!
     var imageRoute: String!
-    var nameRoute: String!
+    var nameCurentPlace: String!
+    var nameCity: String!
+    var nameCountry: String!
+    var starRating: Double!
+    var walkDistance: Double!
+    var timeExcursions: Double!
+    var headingTextRoute: String!
     var textAboutRoute: String!
-   // var annotationsForRoute = [MKPointAnnotation]()
-    var distanceRoute: Double!
-    
-     
-    init(dictionary: [String: Any]) {
-        
-       self.idUser = dictionary["idUser"] as? String ?? ""
-       self.imageUser = dictionary["imageUser"] as? String ?? ""
-       self.nameUser = dictionary["nameUser"] as? String ?? ""
-       self.shurnameUser = dictionary["shurnameUser"] as? String ?? ""
-        
-       self.idRoute = dictionary["idRoute"] as? String ?? ""
-       self.imageRoute = dictionary["imageRoute"] as? String ?? ""
-       self.nameRoute = dictionary["nameRoute"] as? String ?? ""
-       self.textAboutRoute = dictionary["textAboutRoute"] as? String ?? ""
-      //  self.annotationsForRoute = dictionary["annotationsForRoute"] as? [MKPointAnnotation?]()
-       self.distanceRoute = dictionary["distanceRoute"] as? Double ?? nil
-    
-    }
+
 }
 
